bc7a8f89aa2debe955405e52b1e7e2dd109b1742
Ning Xia <nxiamail@gmail.com>
2015-05-13 12:53:59 -0700

    completed functional-classes - queue-specific behavior achieved with reassignment of key values in a for in loop

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,33 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    if(size>0){
+      var result = storage[0];
+      delete storage[0];
+
+      for(var key in storage){
+        var temp = storage[key];
+        storage[key-1] = temp;
+      }
+      size--;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
 };
+
