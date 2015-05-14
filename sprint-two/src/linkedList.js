8234dc1defde0ed65c15d6da389e8a5aba8356a7
ning-github <nxiamail@gmail.com>
2015-05-14 10:41:58 -0700

    completed linkedlist.js - used recursion working on boolean variable

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var linkListNode = Node(value);
+    if(list.head === null) {
+      list.head = linkListNode;
+      list.tail = linkListNode;
+    } else {
+      list.tail.next = linkListNode;
+      list.tail = linkListNode;
+    }
   };
 
   list.removeHead = function(){
+    var newHead = list.head.next;
+    var oldValue = list.head.value;
+    delete list.head;
+    list.head = newHead;
+    return oldValue;
   };
 
   list.contains = function(target){
+    var inside = false;
+    var drill = function (node) {
+      if (node.value === target) {
+        inside = true;
+      } else if (node.next) {
+        drill(node.next);
+      }
+    }
+    return inside;
   };
 
   return list;
