0b8013ee6150ffeb2ce765679e707931eb8aba4a
ning-github <nxiamail@gmail.com>
2015-05-14 20:42:21 -0700

    added comments to solidify understanding of linked list - added time complexity notes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,26 +4,33 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // instantiate an object of Node class
     var linkListNode = Node(value);
+    // if the linked list is empty
     if(list.head === null) {
-      list.head = linkListNode;
-      list.tail = linkListNode;
-    } else {
-      list.tail.next = linkListNode;
-      list.tail = linkListNode;
+      //since with this add, there is only one node
+      list.head = linkListNode;  // it is both head
+      list.tail = linkListNode;  // and tail
+    } else {  // if the list were not empty, then
+      list.tail.next = linkListNode;  // the current tail will now point to the added node
+      list.tail = linkListNode; // the tail label now points to the newly added node
     }
   };
 
   list.removeHead = function(){
+    // store what the old head is currently pointing to (second node, soon to be first)
     var newHead = list.head.next;
+    // store the soon-to-be-gone head's value (for return purposes)
     var oldValue = list.head.value;
     delete list.head;
+    // following deletion, the list.head label now points to what used to be second
     list.head = newHead;
     return oldValue;
   };
 
   list.contains = function(target){
     var inside = false;
+    // recursively drill down each link level to see if there is match
     var drill = function (node) {
       if (node.value === target) {
         inside = true;
@@ -48,5 +55,8 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *   add         O(1) constant time operation (only needs to move some pointers)
+ *   removeHead  O(n) linear time operation (because it needs to traverse tree to find the removee's preceding node, so that it can reattach that preceding pointer)
+ *   contains    O(n) linear time operation (needs to traverse to find match)
  */
 
