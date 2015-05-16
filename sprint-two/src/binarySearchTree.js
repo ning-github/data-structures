e468d782a643805f85dfdeb1825f719cc70e5adb
Ning Xia <nxiamail@gmail.com>
2015-05-16 14:10:52 -0700

    important to check that tree is NOT null - otherwise an error occurs due to trying to access a proerty on a null recursion is used for all cases - in contains and depthFirstLog, created a recursive helper function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,66 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  if (value > this.value){
+    if (this.right === null){
+      this.right = new BinarySearchTree(value);
+    }
+    else{
+      this.right.insert(value);
+    }
+  }
+  else if (value < this.value){
+    if (this.left === null){
+      this.left = new BinarySearchTree(value);
+    }
+    else{
+      this.left.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  var found = false;
+  var recurseSearch = function(tree){
+  // important to check that tree is NOT null, otherwise will error for attempted property access
+    if (tree){ 
+      if (tree.value === value){
+        found = true;
+      }
+      else if (value > tree.value){
+        recurseSearch(tree.right);
+      }
+      else {
+        recurseSearch(tree.left);
+      }
+    }
+  };
+  recurseSearch(this);
+  return found;
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  var rescurseTree = function(tree){
+    if (tree){
+      callback(tree.value);
+    }
+    if (tree.left){
+      rescurseTree(tree.left);
+    }
+    if (tree.right){
+      rescurseTree(tree.right);
+    }
+  }
+  rescurseTree(this);
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    add and contains are both O(log(n)) because it is not searching each and every one.
+      - it is making an educated search (the further it searches, it will also know what NOT to search)
+    depthFirstLog is O(n) linear time operation because it is visiting every single input
  */
