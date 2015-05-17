edc2d40752ef0ec53f38923ae38a746451a2ea49
Ning Xia <nxiamail@gmail.com>
2015-05-17 15:24:32 -0700

    created doublyLinkedList.js and .spec (copied from linkedList versions) also reimplemented set.js using hash tables (a set is now essentially a js object)

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,62 @@
+var LinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    // instantiate an object of Node class
+    var linkListNode = Node(value);
+    // if the linked list is empty
+    if(list.head === null) {
+      //since with this add, there is only one node
+      list.head = linkListNode;  // it is both head
+      list.tail = linkListNode;  // and tail
+    } else {  // if the list were not empty, then
+      list.tail.next = linkListNode;  // the current tail will now point to the added node
+      list.tail = linkListNode; // the tail label now points to the newly added node
+    }
+  };
+
+  list.removeHead = function(){
+    // store what the old head is currently pointing to (second node, soon to be first)
+    var newHead = list.head.next;
+    // store the soon-to-be-gone head's value (for return purposes)
+    var oldValue = list.head.value;
+    delete list.head;
+    // following deletion, the list.head label now points to what used to be second
+    list.head = newHead;
+    return oldValue;
+  };
+
+  list.contains = function(target){
+    var inside = false;
+    // recursively drill down each link level to see if there is match
+    var drill = function (node) {
+      if (node.value === target) {
+        inside = true;
+      } else if (node.next) {
+        drill(node.next);
+      }
+    }
+    drill(list.head);
+    return inside;
+  };
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ *   add         O(1) constant time operation (only needs to move some pointers)
+ *   removeHead  O(n) linear time operation (because it needs to traverse tree to find the removee's preceding node, so that it can reattach that preceding pointer)
+ *   contains    O(n) linear time operation (needs to traverse to find match)
+ */
+
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,6 +1,51 @@
 var Set = function(){
   var set = Object.create(setPrototype);
 // _ communicates to other developers not to directly access _storage. (similar to private variables in C++ and getter/setter access instead)
+  
+// the below is a Hash Table impmlementation of set
+  // this is meant to demonstrate how js Objects are essentially
+  //  just sets that are created with hash tables
+
+  // the time complexity of all functions in this set is CONSTANT time
+  //  - O(n)  
+
+  set._storage = new HashTable();
+  return set;
+};
+
+var setPrototype = {};
+
+setPrototype.add = function(item){
+  this._storage.insert(item, item);
+};
+
+setPrototype.contains = function(item){
+  if (this._storage.retrieve(item)){
+    return true;
+  }
+  return false;
+};
+
+setPrototype.remove = function(item){
+  this._storage.remove(item);
+};
+
+
+/*
+ * Complexity: What is the time complexity of the above functions (for Array)?
+ *
+ * add      O(1)  constant time operation (unaffected by how many other properties are stored)
+ * contains O(n)  linear time operation (needs to traverse through collection, so higher the size, higher the work)
+ * remove   O(1)  constant time operation (targeted removal, unaffected by input size)
+ *
+ */
+
+
+ /******* Object implementation. will be refactoring for HashTable implemenetation
+
+var Set = function(){
+  var set = Object.create(setPrototype);
+// _ communicates to other developers not to directly access _storage. (similar to private variables in C++ and getter/setter access instead)
   set._storage = {};
   return set;
 };
@@ -23,13 +68,4 @@ setPrototype.contains = function(item){
 setPrototype.remove = function(item){
   delete this._storage[item];
 };
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- *
- * add      O(1)  constant time operation (unaffected by how many other properties are stored)
- * contains O(n)  linear time operation (needs to traverse through collection, so higher the size, higher the work)
- * remove   O(1)  constant time operation (targeted removal, unaffected by input size)
- *
- */
+*/
